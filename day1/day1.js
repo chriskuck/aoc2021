dd5ae7dcd61a3a954e6809f310812cabc9937e4c
Chris Kuck <chriskuck@gmail.com>
2021-12-01 15:22:25 -0500

    day2, messy and js :(

--- day1/day1.js
+++ day1/day1.js
@@ -4,8 +4,6 @@ if(process.argv.length < 3) {
   console.log("usage: day1.ts <input>")
 }
 
-
-
 contents = fs.readFileSync(process.argv[2],'utf8');
 values = contents.split('\n').map((x) => { return parseInt(x, 10)}).filter(Number)
 
@@ -15,9 +13,25 @@ for(let i=0; i<values.length-1; i++) {
 }
 
 result = pairs.map((e) => e[0] < e[1] ? 1:0).reduce((a,m) => a+m, 0)
-console.log(result)
+console.log("part1:" + result)
 
 
+values = contents.split('\n').map((x) => { return parseInt(x, 10)}).filter(Number)
+
+trios=[]
+for(let i=0; i<values.length-2; i++) {
+  trios.push([values[i], values[i+1], values[i+2]])
+}
+
+values = trios.map((t) => t.reduce((a,m) => a+m, 0))
+
+pairs=[]
+for(let i=0; i<values.length-1; i++) {
+  pairs.push([values[i], values[i+1]])
+}
+
+result = pairs.map((e) => e[0] < e[1] ? 1:0).reduce((a,m) => a+m, 0)
+console.log("part2:" + result)
 // read file from input
 // for each line, make number
 // do a loop for each element -1 comparing last to first
